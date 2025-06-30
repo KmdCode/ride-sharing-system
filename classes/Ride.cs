5c2e94105215fb4b7e8cdaddace3ec545e792e73
karabomodise260@gmail.com <karabomodise260@gmail.com>
2025-06-30 11:30:18 +0200

    Project file structure

---
+++ classes/Ride.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ride_sharing_system.classes
+{
+    internal class Ride
+    {
+    }
+}
